fef08ced1cefe0f994776bed26278fd0e0f1f6e6
radillaowena <radilla_nathaniel_27rpl@student.smktelkom-mlg.sch.id>
2020-12-08 07:12:45 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+
+Route::middleware(['jwt.verify'])->group(function(){
+
+});
+
+
